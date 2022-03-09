c8d5b61519c7469b51302a4d85c061ee4a2675b5
Joshua Sinyor <74914999+JoshSinyor@users.noreply.github.com>
2022-03-09 14:18:29 +0000

    Update counter.rb and counter_spec.rb. Reflects passing of second test.

--- lib/counter.rb
+++ lib/counter.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
 
 def total_results(results)
-  "Green: 1"
+  green = 0
+  results_array = results.split(", ")
+
+  results_array.each do |result|
+    green += 1
+  end
+
+  "Green: #{green}"
 end
--- spec/units/counter_spec.rb
+++ spec/units/counter_spec.rb
@@ -6,4 +6,8 @@ describe 'total_results' do
   it 'counts a single result' do
     expect(total_results("Green")).to eq "Green: 1"
   end
+
+  it 'counts multiple results' do
+    expect(total_results("Green, Green")).to eq "Green: 2"
+  end
 end
